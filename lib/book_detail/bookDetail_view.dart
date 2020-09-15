cb56460a551e67e177f4ade2b60ad3fe8a40a964
fatihemree <fatih.emre.212@gmail.com>
2020-09-15 22:18:25 +0300

    firt commit

---
+++ lib/book_detail/bookDetail_view.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class BookDetailView extends BookDetailViewModel {
+@override
+Widget build(BuildContext context) {
+return Text('Just a placeholder');
+}
+}
---
+++ lib/book_detail/bookDetail_view_model.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+abstract class BookDetailViewModel extends State<BookDetail> {
+
+}
