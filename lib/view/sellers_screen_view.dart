a9318d7bc7759076c8707d49cb45a0a6bf948e4b
edaersu <edaersu1@gmail.com>
2020-09-15 18:48:55 +0300

    sellers screen created

--- lib/view/sellers_screen_view.dart
+++ lib/view/sellers_screen_view.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutterfoodapp/model/sellers_screen_model.dart';
 import 'package:flutterfoodapp/view/sellers_screen.dart';
 import 'package:flutterfoodapp/view_model/sellers_screen_view_model.dart';
+import 'package:flutterfoodapp/core/extensions/future_builder.dart';
 
 class SellersScreenView extends SellersScreenViewModel {
   List<Sellers> sellers = [
-    new Sellers("eda", "ersu", 3, 35),
-    new Sellers("eda", "ersu", 3, 35),
-    new Sellers("eda", "ersu", 3, 35),
-    new Sellers("eda", "ersu", 3, 35),
-    new Sellers("eda", "ersu", 3, 35)
+    new Sellers("Eda", "Ersu", 3, 35),
+    new Sellers("Eda", "Ersu", 3, 35),
+    new Sellers("Eda", "Ersu", 3, 35),
+    new Sellers("Eda", "Ersu", 3, 35),
+    new Sellers("Eda", "Ersu", 3, 35),
+    new Sellers("Eda", "Ersu", 3, 35),
+    new Sellers("Eda", "Ersu", 3, 35),
   ];
 
   @override
@@ -27,7 +30,7 @@ class SellersScreenView extends SellersScreenViewModel {
         ),
         title: Text(
           "Pumpkin Heads",
-          style: TextStyle(color: Colors.black, fontSize: 20),
+          style: TextStyle(color: Colors.black, fontSize: 21),
         ),
         actions: [
           IconButton(
@@ -37,9 +40,105 @@ class SellersScreenView extends SellersScreenViewModel {
           )
         ],
       ),
-      body: Center(
-        child: Center(),
-      ),
+      body: Container(
+          child: Future.value(sellers).toBuild<List<Sellers>>(
+        onSuccess: (data) {
+          return ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: data.length,
+              itemBuilder: (context, index) => sellersCard(data[index]));
+        },
+      )),
     );
   }
+
+  Container sellersCard(Sellers seller) => Container(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          seller.name + " " + seller.surname,
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(seller.km.toString() + " km yakınında"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          seller.price.toString() + "₺",
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                height: 30,
+                                width: 80,
+                                child: RaisedButton(
+                                  child: Text(
+                                    "Teklif Ver",
+                                    style: TextStyle(
+                                        fontSize: 11, color: Colors.green),
+                                  ),
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      side: BorderSide(color: Colors.green)),
+                                  onPressed: () {},
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(left: 5),
+                                child: SizedBox(
+                                  height: 30,
+                                  width: 80,
+                                  child: RaisedButton(
+                                    color: Colors.green,
+                                    child: Text(
+                                      "Satın Al",
+                                      style: TextStyle(
+                                          fontSize: 11, color: Colors.white),
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                color: Colors.grey,
+                height: 20,
+                thickness: 1,
+                endIndent: 0,
+              ),
+            ],
+          ),
+        ),
+      );
 }
