32c335eeb1b09b55b67ceaed5f619cf03a1a7abf
fatihemree <fatih.emre.212@gmail.com>
2020-09-15 22:28:04 +0300

    bookDetail pages create

--- lib/book_detail/book_detail_view.dart
+++ lib/book_detail/book_detail_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'book_detail_view_model.dart';
 class BookDetailView extends BookDetailViewModel {
 @override
 Widget build(BuildContext context) {
--- lib/book_detail/book_detail_view_model.dart
+++ lib/book_detail/book_detail_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'book_detail.dart';
 abstract class BookDetailViewModel extends State<BookDetail> {
 
 }
