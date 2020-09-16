68dd98c5a8648a3c77874253d55d6dbd39fc72e5
alperenkbd <alperen_kabadayi@hotmail.com>
2020-09-16 21:32:51 +0300

    changed design
    changed design

--- lib/category_choose_screen/view/category_choose_view.dart
+++ lib/category_choose_screen/view/category_choose_view.dart
@@ -2,13 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutterfoodapp/category_choose_screen/view_model/category_choose_viewmodel.dart';
 import 'package:circular_check_box/circular_check_box.dart';
 
+Color cardcolorgeneral = Color(0xffD3D3D3);
+List<String> indexList = new List();
+
 class CategoryChooseView extends CategoryChooseViewModel {
-  Items item1 = new Items(title: "Biography", istrue: false);
-  Items item2 = new Items(title: "Business", istrue: false);
-  Items item3 = new Items(title: "Children", istrue: false);
-  Items item4 = new Items(title: "Cookery", istrue: false);
-  Items item5 = new Items(title: "Fiction", istrue: false);
-  Items item6 = new Items(title: "Graphic novels", istrue: false);
+  Items item1 = new Items(
+      title: "Biography", index: 0, istrue: false, cardcolor: cardcolorgeneral);
+  Items item2 = new Items(
+      title: "Business", index: 1, istrue: false, cardcolor: cardcolorgeneral);
+  Items item3 = new Items(
+      title: "Children", index: 2, istrue: false, cardcolor: cardcolorgeneral);
+  Items item4 = new Items(
+      title: "Cookery", index: 3, istrue: false, cardcolor: cardcolorgeneral);
+  Items item5 = new Items(
+      title: "Fiction", index: 4, istrue: false, cardcolor: cardcolorgeneral);
+  Items item6 = new Items(
+      title: "Graphic novels",
+      index: 5,
+      istrue: false,
+      cardcolor: cardcolorgeneral);
   @override
   Widget build(BuildContext context) {
     List<Items> myList = [item1, item2, item3, item4, item5, item6];
@@ -55,7 +67,7 @@ class CategoryChooseView extends CategoryChooseViewModel {
                     style: BorderStyle.solid,
                     width: 1,
                   ),
-                  color: Colors.transparent,
+                  color: Colors.greenAccent,
                   borderRadius: BorderRadius.circular(30.0),
                 ),
                 child: Row(
@@ -65,7 +77,7 @@ class CategoryChooseView extends CategoryChooseViewModel {
                       child: Text(
                         "Send",
                         style: TextStyle(
-                          color: Colors.greenAccent,
+                          color: Colors.white,
                           fontFamily: 'Montserrat',
                           fontSize: 16,
                           fontWeight: FontWeight.w600,
@@ -86,37 +98,56 @@ class CategoryChooseView extends CategoryChooseViewModel {
   Expanded categoryexpanded(List<Items> myList) {
     return Expanded(
         child: GridView.count(
-      childAspectRatio: 1.0,
-      padding: EdgeInsets.only(left: 40, right: 40),
-      crossAxisCount: 2,
-      crossAxisSpacing: 24,
-      mainAxisSpacing: 18,
+      childAspectRatio: 3.0,
+      padding: EdgeInsets.only(left: 5, right: 5),
+      crossAxisCount: 1,
+      crossAxisSpacing: 18,
+      mainAxisSpacing: 5,
       children: myList.map((data) {
         return Material(
-          child: Container(
-            decoration: BoxDecoration(
-                color: Color(0xff747474),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircularCheckBox(
-                    value: data.istrue,
-                    materialTapTargetSize: MaterialTapTargetSize.padded,
-                    activeColor: Colors.greenAccent,
-                    onChanged: (value) {
-                      setState(() {
-                        data.istrue = value;
-                        print(data.istrue);
-                      });
-                    }),
-                Text(
-                  data.title,
-                  style: TextStyle(
-                      fontFamily: "Roboto", fontSize: 15, color: Colors.white),
-                )
-              ],
+          child: new InkWell(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: data.cardcolor,
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularCheckBox(
+                      value: data.istrue,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      activeColor: Color(0XFF089000),
+                      onChanged: (value) {
+                        setState(() {
+                          // data.istrue = value;
+                          // print(data.istrue);
+                        });
+                      }),
+                  Text(
+                    data.title,
+                    style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 15,
+                        color: Colors.white),
+                  )
+                ],
+              ),
             ),
+            onTap: () {
+              setState(() {
+                if (data.istrue == false) {
+                  data.cardcolor = Color(0xff90EE90);
+                  data.istrue = true;
+                  indexList.add(data.title);
+                } else {
+                  data.cardcolor = Color(0xffD3D3D3);
+                  data.istrue = false;
+                  indexList.removeAt(data.index);
+                }
+
+                print(data.istrue);
+              });
+            },
           ),
         );
       }).toList(),
@@ -127,6 +158,7 @@ class CategoryChooseView extends CategoryChooseViewModel {
 class Items {
   String title;
   bool istrue;
-
-  Items({this.title, this.istrue});
+  int index;
+  Color cardcolor;
+  Items({this.title, this.index, this.istrue, this.cardcolor});
 }
