657617ce0ea24394e37f6fc19e28b11c4f02f660
edaersu <edaersu1@gmail.com>
2020-09-17 20:16:14 +0300

    push barcode to book detail page

--- lib/app/views/search_screen/search_screen_view_model.dart
+++ lib/app/views/search_screen/search_screen_view_model.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfoodapp/core/init/navigation/navigation_service.dart';
 import 'search_screen.dart';
 
-abstract class SearchScreenViewModel extends State<SearchScreen> {}
+abstract class SearchScreenViewModel extends State<SearchScreen>
+    with BaseViewModel {}
+
+abstract class BaseViewModel {
+  NavigationService navigation = NavigationService.instance;
+}
